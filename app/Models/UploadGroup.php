892a1c2717d6a66cd3611981fae4e0f6bf079b07
keymetaphore <keymetaphore@users.noreply.github.com>
2020-12-18 21:21:10 +0000

    Apply fixes from StyleCI

--- app/Models/UploadGroup.php
+++ app/Models/UploadGroup.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class UploadGroup extends Model
 {
     use HasFactory;
-    protected $fillable = [ 'uniqueid', 'author' ];
+    protected $fillable = ['uniqueid', 'author'];
 
     public function images()
     {
