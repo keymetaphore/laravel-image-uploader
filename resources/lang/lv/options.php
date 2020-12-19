be467f05ce5b51b721cae13a4995256d18164253
keymetaphore <40383576+keymetaphore@users.noreply.github.com>
2020-12-19 13:24:05 +0200

    Added Latvian translation

---
+++ resources/lang/lv/options.php
@@ -0,0 +1,9 @@
+<?php
+
+    return [
+        'header'              => 'Uzstādījumi',
+        'api_key'             => 'Tava API atslēga',
+        'api_key_toggle'      => 'Parādīt atslēgu',
+        'sharex_instructions' => 'ShareX uzstādīšanas instrukcijas:',
+        'sharex_install'      => 'Pārliecinies, ka vērtības sakrīt ar augstāk redzamajām!',
+    ];
